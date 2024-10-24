36b431140e5f4ee62191aab4271bd4dd678cb74f
Muhammadzishan203 <zishanqureshi203@gmail.com>
2024-10-25 03:19:21 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],  // Paths to your template files
+  theme: {
+    extend: {},  // Extend default theme (optional)
+  },
+  plugins: [],  // Add any plugins here
+}
